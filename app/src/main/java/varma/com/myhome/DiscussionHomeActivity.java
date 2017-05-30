9c849ca3a6a63ba77009c965cc6b0cde69e6b3f9
Varma <varma.rk@hotmail.com>
2017-05-30 13:57:11 +0530

    Commit1

--- app/src/main/java/varma/com/myhome/DiscussionHomeActivity.java
+++ app/src/main/java/varma/com/myhome/DiscussionHomeActivity.java
@@ -3,6 +3,10 @@ package varma.com.myhome;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -12,10 +16,23 @@ public class DiscussionHomeActivity extends AppCompatActivity {
     GridView gridView;
     String letterList[] = {"", "", "", "", "", ""};
     int lettersIcon[] = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f};
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discussion_home);
+
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Discussions");
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
 
         gridView = (GridView) findViewById(R.id.gridView);
         GridAdapter adapter = new GridAdapter(DiscussionHomeActivity.this, lettersIcon, letterList);
@@ -33,5 +50,12 @@ public class DiscussionHomeActivity extends AppCompatActivity {
             }
 
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 }
