edb52b86a6bd802c2b53911341bf9c7f62619df5
Varma <varma.rk@hotmail.com>
2017-05-31 16:47:03 +0530

    Commit-31051650

--- app/src/main/java/varma/com/app/AppConfig.java
+++ app/src/main/java/varma/com/app/AppConfig.java
@@ -1,22 +1,26 @@
 package varma.com.app;
 
 public class AppConfig {
-	// Server user login url
-	// public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
-
-	public static String URL_LOGIN = "http://192.168.3.2/LogIn/login.php";
-
-	// Server user register url
-//	public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
-	public static String URL_REGISTER = "http://192.168.3.2/LogIn/register.php";
+/*
+	   public static String URL_LOGIN = "http://192.168.3.2/LogIn/login.php";
+    public static String URL_REGISTER = "http://192.168.3.2/LogIn/register.php";
 
 	public static String URL_REGISTERDEVICE = "http://192.168.3.2/FcmSimplifiedCoding/RegisterDevice.php";
-	public static String URL_IMAGES = "http://192.168.3.2/myImages.php";
 
+    public static String URL_IMAGES = "http://192.168.3.2/myImages.php";
 	public static String URL_DIS_UPDATE = "http://192.168.3.2/mhConversationsUpdate.php";
 	public static String URL_DIS = "http://192.168.3.2/mhConversations.php";
 	public static String URL_DIS_REPLY = "http://192.168.3.2/mhDiscussionViewReply.php";
+*/
 
+    public static String URL_LOGIN = "http://varmaserver.bitnamiapp.com/varmaphp/LogIn/login.php";
+    public static String URL_REGISTER = "http://varmaserver.bitnamiapp.com/varmaphp/LogIn/register.php";
 
+    public static String URL_REGISTERDEVICE = "http://varmaserver.bitnamiapp.com/varmaphp/FcmSimplifiedCoding/RegisterDevice.php";
+
+    public static String URL_IMAGES = "http://varmaserver.bitnamiapp.com/varmaphp/myImages.php";
+    public static String URL_DIS_UPDATE = "http://varmaserver.bitnamiapp.com/varmaphp/mhConversationsUpdate.php";
+    public static String URL_DIS = "http://varmaserver.bitnamiapp.com/varmaphp/mhConversations.php";
+    public static String URL_DIS_REPLY = "http://varmaserver.bitnamiapp.com/varmaphp/mhDiscussionViewReply.php";
 
 }
